923e2780e536926ef7d8001e18dc940f74f46969
Dhiraj275 <dhirajprajapati.co@gmail.com>
2025-12-07 15:09:35 +0530

    fix: logging timestamp month

--- src/log.c
+++ src/log.c
@@ -16,7 +16,7 @@ void get_time() {
 
 void dv_log(LOG_LEVEL lvl, const char *fmt, ...) {
   get_time();
-  printf("[%d:%d:%d %d:%d:%d] [%s] ", m_time->tm_mday, m_time->tm_mon,
+  printf("[%d:%d:%d %d:%d:%d] [%s] ", m_time->tm_mday, m_time->tm_mon + 1,
          m_time->tm_year + 1900, m_time->tm_hour, m_time->tm_min,
          m_time->tm_sec, log_type[lvl]);
   
